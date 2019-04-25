c2cb7988206f1d01e0d7e42969c7bc385ab0d4d8
JaySebagh <jaysebagh@gmail.com>
2019-04-25 14:41:27 -0700

    setup

---
+++ mealprep/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Todo from './components/breakfast/todo.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
