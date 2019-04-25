049b75f60dd045a06c9f9c3569dd610983422ad3
JaySebagh <jaysebagh@gmail.com>
2019-04-25 14:47:03 -0700

    started adding different sections

--- mealprep/src/App.js
+++ mealprep/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
-import Todo from './components/breakfast/todo.js'
+import BreakfastTodo from './components/breakfast/todo.js';
+import LunchTodo from './components/lunch/todo.js';
+import DinnerTodo from './components/dinner/todo.js';
+import SnackTodo from './components/snack/todo.js';
 
 function App() {
   return (
     <div className="App">
-      <Todo />
+      <BreakfastTodo />
+      <LunchTodo />
+      <DinnerTodo /> 
+      <SnackTodo />
     </div>
   );
 }
--- mealprep/src/components/snack/todo.js
+++ mealprep/src/components/snack/todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Todo extends React.Component {
     render() {
         return (
-          <div>Breakfast</div>
+          <div>Snack</div>
         );
       }
     }
